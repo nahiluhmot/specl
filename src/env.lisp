83d0a736baeef604cb4878bcb9a1571fefb80e4c
Tom Hulihan <hulihan.tom159@gmail.com>
2013-04-28 14:31:27 -0400

    messages => desc

--- src/env.lisp
+++ src/env.lisp
@@ -1,12 +1,12 @@
 (in-package #:specl)
 
 ;; Create a new environment that holds all of the metadata for a test context.
-(defun new-env (&key messages befores afters defs expectations children)
-  (list 'ENV messages befores afters defs expectations children))
+(defun new-env (&key desc befores afters defs expectations children)
+  (list 'ENV desc befores afters defs expectations children))
 
 ;; Anaphoric macro to access each element in an environment.
 (defmacro with-env (env &body body)
-  `(dbind (messages befores afters defs expectations children) (cdr ,env)
+  `(dbind (desc befores afters defs expectations children) (cdr ,env)
     ,@body))
 
 ;; Test if a value is an env.
@@ -14,11 +14,13 @@
   (and (listp env)
        (= 7 (length env))
        (eq 'ENV (car env))
-       (every #'listp (cdr env))))
+       (stringp (cadr env))
+       (every #'listp (cddr env))))
 
 ;; Merge two envs together.
 (defun env+ (env-a env-b)
-  `(ENV ,@(mapcar (lambda (a b) (concatenate 'list a b))
+  `(ENV ,(concatenate 'string (cadr env-a) (cadr env-b))
+        ,@(mapcar (lambda (a b) (concatenate 'list a b))
                   (cdr env-a)
                   (cdr env-b))))
 
