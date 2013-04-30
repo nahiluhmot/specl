913e6fb6218d0fd3b648a279971ae02bc344308f
Todd Lunter <tlunter@gmail.com>
2013-04-30 15:58:21 -0400

    Added the ability to define named functions in the context by using (def ...)

--- src/env.lisp
+++ src/env.lisp
@@ -1,18 +1,18 @@
 (in-package #:specl)
 
 ;; Create a new environment that holds all of the metadata for a test context.
-(defun new-env (&key (desc "") befores afters lets expectations children)
-  (list 'ENV desc befores afters lets expectations children))
+(defun new-env (&key (desc "") befores afters defs lets expectations children)
+  (list 'ENV desc befores afters defs lets expectations children))
 
 ;; Anaphoric macro to access each element in an environment.
 (defmacro with-env (env &body body)
-  `(dbind (desc befores afters lets expectations children) (cdr ,env)
+  `(dbind (desc befores afters defs lets expectations children) (cdr ,env)
     ,@body))
 
 ;; Test if a value is an env.
 (defun env? (env)
   (and (listp env)
-       (= 7 (length env))
+       (= 8 (length env))
        (eq 'ENV (car env))
        (stringp (cadr env))
        (every #'listp (cddr env))))
