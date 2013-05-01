dd0eb32604fef9349aadcae472c6f68bd31dea47
Tom Hulihan <hulihan.tom159@gmail.com>
2013-05-01 18:21:47 -0400

    added `lazy-let` and added it to the context expansion

--- src/package.lisp
+++ src/package.lisp
@@ -1,7 +1,7 @@
 (defpackage #:specl
   (:use #:cl)
   (:export ; src/util 
-           #:alias #:dbind
+           #:alias #:dbind #:lazy-let
            ; src/env
            #:new-env #:with-env #:env? #:env+ #:inherit
            ; src/context
--- src/util.lisp
+++ src/util.lisp
@@ -9,3 +9,23 @@
 ;; macro.
 (alias dbind destructuring-bind)
 
+(labels ((lazy-let-expand-one (arg body)
+           (dbind (sym . value) arg
+             (let ((sym-set? (gensym))
+                   (sym-val  (gensym)))
+               `(let ((,sym-set? nil)
+                      (,sym-val  nil))
+                 (symbol-macrolet ((,sym (if ,sym-set?
+                                           ,sym-val
+                                           (progn (setf ,sym-set? t)
+                                                  (setf ,sym-val (progn ,@value))))))
+                   ,body))))))
+  ;; Acts much like let, with the expection that all of the arguments are
+  ;; evaluated lazily and then cached.
+  (defmacro lazy-let (definitions &body body)
+    (if (null definitions)
+      body
+      (reduce #'lazy-let-expand-one
+              definitions
+              :initial-value `(progn ,@body)
+              :from-end t))))
