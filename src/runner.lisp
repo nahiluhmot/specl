cbcf2b02447fef339896f53e5767ade4cacee6fa
Tom Hulihan <hulihan.tom159@gmail.com>
2013-05-11 12:50:12 -0400

    cleaned up the runner

--- src/runner.lisp
+++ src/runner.lisp
@@ -2,37 +2,37 @@
 
 (declaim #+sbcl (sb-ext:muffle-conditions style-warning))
 
-(defun expand-it (it befores afters funcs macros lets)
+(defun expand-it (desc it befores afters funcs macros lets)
   "Given an expectation and its necessary callbacks, expands them into Common
 Lisp. The code will return `t` on success and `nil` on failure."
-  `(handler-case
-     (progn
-       (lazy-let ,lets
-         (macrolet ,macros
-           (labels ,funcs
-             ,@befores
-             ,@it
-             ,@afters)))
-       t)
-     (error (e) nil)))
+  (let ((expanded-desc (gensym)) (e (gensym)))
+    `(handler-case
+       (progn
+         (lazy-let ,lets
+           (macrolet ,macros
+             (labels ,funcs
+               ,@befores
+               ,@(cdr it)
+               ,@afters)))
+         (push ,desc *passes*))
+       (error (,e)
+         (let ((,expanded-desc (format nil "~A: ~A" ,desc ,e)))
+         (push ,expanded-desc *failures*))))))
 
 (defun expand-env (env &optional (parent-desc ""))
   "Given an environment, expands it into Common Lisp code."
-  (let ((passes (gensym)) (failures (gensym)))
-    (with-env env
-      `(progn
-         ,@(mapcar
-             (lambda (it)
-               (let ((full-desc (string-trim " " (concatenate 'string parent-desc " " desc " " (car it)))))
-                 `(if ,(expand-it (cdr it) befores afters funcs macros lets)
-                   (push ,full-desc *passes*)
-                   (push ,full-desc *failures*))))
-             expectations)
-         ,@(mapcar
-             (lambda (child)
-               (expand-env (inherit env child)
-                           (string-trim " " (concatenate 'string parent-desc " " desc))))
-             children)))))
+  (with-env env
+    `(progn
+       ,@(mapcar
+           (lambda (it)
+             (let ((full-desc (string-trim " " (concatenate 'string parent-desc " " desc " " (car it)))))
+               (expand-it full-desc (cdr it) befores afters funcs macros lets)))
+           expectations)
+       ,@(mapcar
+           (lambda (child)
+             (expand-env (inherit env child)
+                         (string-trim " " (concatenate 'string parent-desc " " desc))))
+           children))))
 
 (defmacro run! ()
   (let ((desc (gensym)))
